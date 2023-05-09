a416d8a8aad03d9b00a7e7fbfb0a776089b885b7
GunJaaI <110761756+GunJaaI@users.noreply.github.com>
2023-05-09 12:36:53 +0700

    09/05/66

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -55,7 +55,12 @@ public class Health : MonoBehaviour {
         }
         anim.SetTrigger("Heal");
 
-        bool overMaxHealth = (health + amount > MAX_HEALTH) ;
+        bool overMaxHealth = (health + amount > MAX_HEALTH);
+
+        RectTransform textTransform = Instantiate(healthText).GetComponent<RectTransform>();
+        textTransform.transform.position = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+        textTransform.SetParent(canvas.transform);
 
         if (overMaxHealth) {
             this.health = MAX_HEALTH; // when over heal
